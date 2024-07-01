764697741b2cf5ff02f0401642db2a6996d96b91
joshutik <utik2.4real@gmail.com>
2024-07-01 18:32:37 +0100

    added some new components

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,11 @@
 // import React from 'react'
 import NavigationBar from '../NavigationBar/Navbar'
 import HeroSection from '../HeroSection/Heropage'
-// import Services from "../Services/Services";
-// import Review from '../Review/Review';
-// import Faq from '../FAQ/Faq';
+import Services from "../Services/Services";
+import Review from '../Review/Review';
+import Faq from '../FAQ/Faq';
 // import ContactUs from '../ContactUs/Contact';
-// import CallToAction from '../CallToAction/Calltoaction';
+import CallToAction from '../CallToAction/Calltoaction';
 // import Footer from '../Footer/Footer';
 
 const Main = () => {
@@ -13,14 +13,14 @@ const Main = () => {
     <div>
         <NavigationBar/>
         <HeroSection/>
-        {/* <Services/> */}
-        {/* <Review/> */}
-        {/* <Faq/> */}
+        <Services/>
+        <Review/>
+        <Faq/>
         {/* <ContactUs/> */}
-        {/* <CallToAction/> */}
+        <CallToAction/>
         {/* <Footer/> */}
     </div>
   )
 }
 
-export default Main
+export default Main;
--- src/Components/NavigationBar/Navbar.jsx
+++ src/Components/NavigationBar/Navbar.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import "./NavBar.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo1 from "../assets/Logo.png";
 import { Link } from "react-router-dom";
 
