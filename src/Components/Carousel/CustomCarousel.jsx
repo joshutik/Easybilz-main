91ffc90ba532be479d1928cf57c94d8eb9dc3e0d
joshutik <utik2.4real@gmail.com>
2024-07-07 23:45:41 +0100

    fixed the arrousel

--- src/Components/Carousel/CustomCarousel.jsx
+++ src/Components/Carousel/CustomCarousel.jsx
@@ -1,7 +1,7 @@
 import BootstrapCarousel from 'react-bootstrap/Carousel';
 import image1 from '../assets/Contact img.png';
-import image2 from '../assets/taufiq-triadi-bHJ3jT-nIrI-unsplash.jpg';
-import image3 from '../assets/survey-form-background (1).jpeg';
+// import image2 from '../assets/taufiq-triadi-bHJ3jT-nIrI-unsplash.jpg';
+// import image3 from '../assets/survey-form-background (1).jpeg';
 
 
 const CustomCarousel = () => {
@@ -13,33 +13,33 @@ const CustomCarousel = () => {
           src={image1}
           alt="First slide"
         />
-        <BootstrapCarousel.Caption>
+        {/* <BootstrapCarousel.Caption>
           <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </BootstrapCarousel.Caption>
+        </BootstrapCarousel.Caption> */}
       </BootstrapCarousel.Item>
-      <BootstrapCarousel.Item>
-        <img
+      {/* <BootstrapCarousel.Item> */}
+        {/* <img
           className="d-block w-100"
           src={image2}
           alt="Second slide"
-        />
-        <BootstrapCarousel.Caption>
+        /> */}
+        {/* <BootstrapCarousel.Caption>
           <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </BootstrapCarousel.Caption>
-      </BootstrapCarousel.Item>
-      <BootstrapCarousel.Item>
+        </BootstrapCarousel.Caption> */}
+      {/* </BootstrapCarousel.Item> */}
+      {/* <BootstrapCarousel.Item>
         <img
           className="d-block w-100"
           src={image3}
           alt="Third slide"
-        />
-        <BootstrapCarousel.Caption>
+        /> */}
+        {/* <BootstrapCarousel.Caption>
           <h5>Third slide label</h5>
           <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </BootstrapCarousel.Caption>
-      </BootstrapCarousel.Item>
+        </BootstrapCarousel.Caption> */}
+      {/* </BootstrapCarousel.Item> */}
     </BootstrapCarousel>
   );
 };
