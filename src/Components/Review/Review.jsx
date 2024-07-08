da016446cfc24143dd081f9a02d02c2b632a6635
joshutik <utik2.4real@gmail.com>
2024-07-08 09:02:02 +0100

    made the changes to the review videos

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -8,8 +8,12 @@ const Review = () => {
         <h2 className="fw-bold">Member’s Review</h2>
         <p>
           We are 100% trustworthy that’s why our members only have great things
-          to say about us. <Link to={"/register"} href="#">Join us</Link> today and secure your future through our
-          savings and investment feature.
+          to say about us.{" "}
+          <Link to={"/register"} href="#">
+            Join us
+          </Link>{" "}
+          today and secure your future through our savings and investment
+          feature.
         </p>
       </div>
       <div className="container-fluid">
@@ -17,7 +21,8 @@ const Review = () => {
           <div className="col-lg-4 col-md-6 col-sm-12">
             <div className="card border-0">
               <Ratio aspectRatio="4x3" className="rounded-5">
-                <iframe className="rounded-5"
+                {/* <iframe
+                  className="rounded-5 btn disabled border-0"
                   width="853"
                   height="480"
                   src="https://www.youtube.com/embed/3rPIp11cy54"
@@ -26,6 +31,16 @@ const Review = () => {
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
+                ></iframe> */}
+                <iframe className="rounded-5 btn disabled border-0"
+                  width="853"
+                  height="480"
+                  src="https://www.youtube.com/embed/fuiiJuB7tJs"
+                  title="Loans 101 (Loan Basics 1/3)"
+                  // frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  // referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
                 ></iframe>
               </Ratio>
             </div>
@@ -33,7 +48,7 @@ const Review = () => {
           <div className="col-lg-4 col-md-6 col-sm-12">
             <div className="card border-0">
               <Ratio aspectRatio="4x3">
-                <iframe className="rounded-5"
+                {/* <iframe className="rounded-5 btn disabled border-0"
                   width="853"
                   height="480"
                   src="https://www.youtube.com/embed/wAZzyt3goVo"
@@ -42,6 +57,17 @@ const Review = () => {
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen
+                ></iframe> */}
+                <iframe
+                  className="rounded-5 btn disabled border-0"
+                  width="853"
+                  height="480"
+                  src="https://www.youtube.com/embed/v-mlEQ7KW5Q"
+                  title="Savings Goals for Kids | What are Savings goals? |Financial education for kids | Teaching money kids"
+                  // frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  // referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
                 ></iframe>
               </Ratio>
             </div>
