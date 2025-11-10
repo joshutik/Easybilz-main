ee5c7541360244a7a17508cbdd3675eb65cba84f
joshutik <utik2.4real@gmail.com>
2025-11-10 14:26:54 +0100

    fixed the error

--- src/Components/ExistingMemberNavBar/MembershipNavbar.jsx
+++ src/Components/ExistingMemberNavBar/MembershipNavbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import "./MembershipNavBar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo1 from "../assets/Logo.png";
@@ -9,62 +9,12 @@ const MembershipNavbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [userName, setUserName] = useState('');
 
-    // Our Services dropdown
-  const [isServicesDropdownOpen, setIsServicesDropdownOpen] = useState(false);
-
-  
-  // Nested dropdowns inside Our Services
-  const [activeSubDropdown, setActiveSubDropdown] = useState(null);
-
-   const dropdownRef = useRef(null);
-  
-    const handleNavToggle = () => setIsNavOpen(!isNavOpen);
-  
-  
-    const handleLinkClick = () => {
-      setIsNavOpen(false);
-      setIsAboutDropdownOpen(false);
-      setIsServicesDropdownOpen(false);
-      setActiveSubDropdown(null);
-    };
-  
-    const toggleAboutDropdown = () => {
-      setIsAboutDropdownOpen(!isAboutDropdownOpen);
-      setIsServicesDropdownOpen(false);
-    };
-  
-    const toggleServicesDropdown = () => {
-      setIsServicesDropdownOpen(!isServicesDropdownOpen);
-      setIsAboutDropdownOpen(false);
-    };
-  
-    
-  
-    const toggleSubDropdown = (dropdownName) => {
-      setActiveSubDropdown(activeSubDropdown === dropdownName ? null : dropdownName);
-    };
-  
-      // ✅ Close dropdown when clicking outside
-
   useEffect(() => {
     // Retrieve the user's first name from localStorage
     const firstName = localStorage.getItem('firstName');
     if (firstName) {
       setUserName(firstName);
     }
-
-        const handleClickOutside = (event) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-          setIsAboutDropdownOpen(false);
-          setIsServicesDropdownOpen(false);
-          setActiveSubDropdown(null);
-        }
-      };
-  
-      document.addEventListener('mousedown', handleClickOutside);
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-      };
   }, []);
 
   const toggleDropdown = () => {
@@ -105,154 +55,10 @@ const MembershipNavbar = () => {
                   About Us
                 </a>
               </li>
-              {/* OUR SERVICES DROPDOWN */}
-              <li className={`nav-item dropdown ${isServicesDropdownOpen ? 'show' : ''}`}>
-                <button
-                  className="nav-link dropdown-toggle text-light bg-transparent border-0"
-                  onClick={toggleServicesDropdown}
-                >
-                  Our Services
-                </button>
-
-                <ul className={`dropdown-menu ${isServicesDropdownOpen ? 'show' : ''}`}>
-                  {/* Sub-dropdown: Invest with us */}
-                  <li className={`dropdown-submenu ${activeSubDropdown === 'invest' ? 'show' : ''}`}>
-                    <button className="dropdown-item dropdown-toggle" onClick={() => toggleSubDropdown('invest')}>
-                      Invest with Us
-                    </button>
-                    <ul className={`dropdown-menu ${activeSubDropdown === 'invest' ? 'show' : ''}`}>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="investmentPlans"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          <InvestCard />
-                        </ScrollLink>
-                      </li>
-
-                    </ul>
-                  </li>
-
-                  {/* Sub-dropdown: subscription */}
-                  <li className={`dropdown-submenu ${activeSubDropdown === 'grow' ? 'show' : ''}`}>
-                    <button className="dropdown-item dropdown-toggle" onClick={() => toggleSubDropdown('grow')}>
-                      Easybillz subscription
-                    </button>
-                    <ul className={`dropdown-menu ${activeSubDropdown === 'grow' ? 'show' : ''}`}>
-                      {/* <p>
-                                      Never Miss a Payment Again. Simplify your life with Stress-Free Bill Automation. Choose the recurring fees you want us to pay automatically.
-                                  </p> */}
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="loanProcess"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Cable TV
-                        </ScrollLink>
-                      </li>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="loanProcess"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Electricity Bills
-                        </ScrollLink>
-                      </li>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="loanProcess"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Shop for the house
-                        </ScrollLink>
-                      </li>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="eligibility"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Office materials and equipments
-                        </ScrollLink>
-                      </li>
-                    </ul>
-                  </li>
-                  {/* Sub-dropdown: Grow Your Business */}
-                  <li className={`dropdown-submenu ${activeSubDropdown === 'grow' ? 'show' : ''}`}>
-                    <button className="dropdown-item dropdown-toggle" onClick={() => toggleSubDropdown('grow')}>
-                      Grow Your Business (SME Loans)
-                    </button>
-                    <ul className={`dropdown-menu ${activeSubDropdown === 'grow' ? 'show' : ''}`}>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="loanProcess"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Loan Process
-                        </ScrollLink>
-                      </li>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="eligibility"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Eligibility Criteria
-                        </ScrollLink>
-                      </li>
-                    </ul>
-                  </li>
-
-                  {/* Sub-dropdown: Partner with Us */}
-                  <li className={`dropdown-submenu ${activeSubDropdown === 'partner' ? 'show' : ''}`}>
-                    <button className="dropdown-item dropdown-toggle" onClick={() => toggleSubDropdown('partner')}>
-                      Partner with Us (Easybillz Guilder)
-                    </button>
-                    <ul className={`dropdown-menu ${activeSubDropdown === 'partner' ? 'show' : ''}`}>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="affiliate"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Become an Affiliate
-                        </ScrollLink>
-                      </li>
-                      <li>
-                        <ScrollLink
-                          className="dropdown-item"
-                          to="benefits"
-                          smooth
-                          duration={500}
-                          onClick={handleLinkClick}
-                        >
-                          Partner Benefits
-                        </ScrollLink>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#services">
+                  What we do
+                </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link text-light" href="#contact">
