1158700e5e79337508430cecfb273f64eef3daf3
joshutik <utik2.4real@gmail.com>
2025-11-10 14:34:00 +0100

    fixed irresponsivesness

--- src/Components/InvestCard/InvestCard.jsx
+++ src/Components/InvestCard/InvestCard.jsx
@@ -5,7 +5,7 @@ import './InvestCard.css';
 const InvestCard = () =>  {
   return (
     <div>
-        <div className="">
+        <div className="text-center w-75 mx-auto">
             <div className='text-center'>
                 <img src={img1} alt="" />
             </div>
@@ -14,7 +14,7 @@ const InvestCard = () =>  {
             </p>
 
             <div className='text-center'>
-                <button className='border border-0 rounded-pill px-3 text-light invst-btn'>Download</button>
+                <button className='border border-0 rounded-pill px-3 py-1 text-light invst-btn'>Download</button>
             </div>
         </div>
     </div>
